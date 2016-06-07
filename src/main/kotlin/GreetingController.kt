93cfe4586e15a944d478d116a2cd4f29ad4cb773
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-06-07 16:21:31 +1000

    first commit

---
+++ src/main/kotlin/GreetingController.kt
@@ -0,0 +1,11 @@
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "Greeting", value = "/greeting")
+class GreetingController : HttpServlet() {
+    override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
+        res.writer.write("Hello, World from greeting in kotlin!")
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: piyushchauhan
+  Date: 7/06/2016
+  Time: 3:33 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>My Kotlin Gradle Servlet Test</title>
+  </head>
+  <body>
+      <h1>Kotlin Servlet Test with Gradle support!</h1>
+  </body>
+</html>
